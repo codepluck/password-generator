8800d124076d46fdc85bbe9564391695f39746e4
codepluck <developer.dimpythakur@gmail.com>
2025-04-01 23:35:56 +0530

    default-options
    default-options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import PasswordGenerator from "./_password-generator";
 
 export default function Home() {
@@ -6,6 +5,10 @@ export default function Home() {
     <section className="flex flex-col h-screen w-full justify-center items-center">
       <PasswordGenerator
         length={10}
+        includeUppercase={true}
+        includeLowercase={true}
+        includeNumbers={true}
+        includeSymbols={true}
       />
     </section>
   );
